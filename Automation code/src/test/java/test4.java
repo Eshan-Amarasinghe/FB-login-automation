454c3f873904c3178ca4bd960d127952adffd87f
Eshan-Amarasinghe <eshankavishka72@gmail.com>
2025-08-01 15:03:53 +0530

    automate

---
+++ Automation code/src/test/java/test4.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class test4 {
+
+    @Test
+    public void testemaillblank() throws InterruptedException {
+
+        //Initialize web driver
+        WebDriver driver = new ChromeDriver();
+        //Navigate to fb login page
+        driver.get ("https://www.facebook.com/");
+        // Find and fill the email
+        driver.findElement(By.id("email")).sendKeys("");
+        // Find and fill the password
+        driver.findElement(By.name("pass")).sendKeys("johnmarston");
+        Thread.sleep(5000);
+        // Find and click the login button
+        driver.findElement(By.name("login")).click();
+        //Display a message of success
+        System.out.println("Logged in successfully!");
+
+    }
+
+
+}
---
+++ Automation code/src/test/java/test5.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class test5 {
+
+    @Test
+    public void testpasswordblank() throws InterruptedException {
+
+        //Initialize web driver
+        WebDriver driver = new ChromeDriver();
+        //Navigate to fb login page
+        driver.get ("https://www.facebook.com/");
+        // Find and fill the email
+        driver.findElement(By.id("email")).sendKeys("johnmilton6464@gmail.com");
+        // Find and fill the password
+        driver.findElement(By.name("pass")).sendKeys("");
+        Thread.sleep(5000);
+        // Find and click the login button
+        driver.findElement(By.name("login")).click();
+        //Display a message of success
+        System.out.println("Logged in successfully!");
+
+    }
+
+
+}
